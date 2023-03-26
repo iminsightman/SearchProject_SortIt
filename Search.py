78c6ab22b8575de6f475d05ab18353004970209c
elhamarmin <elhmarmin@gmail.com>
2023-03-26 18:13:32 +0430

    dfs , dfs1 and ucs

--- Search.py
+++ Search.py
@@ -19,3 +19,55 @@ class Search:
                     return Solution(c, prb, start_time)
                 queue.append(c)
         return None
+
+   def dfs(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        stack = []
+        state = prb.initState
+        stack.append(state)
+        while len(stack) > 0:
+            state = stack.pop()
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                stack.append(c)
+        return None
+
+    def dfs1(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        stack = []
+        state = prb.initState
+        visited = set()
+        stack.append(state)
+        visited.add(state.__hash__())
+        while len(stack) > 0:
+            state = stack.pop()
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if c.__hash__ not in visited:
+                    visited.add(c.__hash__)
+                    if prb.is_goal(c):
+                        return Solution(c, prb, start_time)
+                    stack.append(c)
+        return None
+
+
+    def ucs(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        queue = []
+        state = prb.initState
+        visited = set()
+        heapq.heappush(queue, (state.g_n, state))
+        visited.add(state.__hash__())
+        while len(queue) > 0:
+            _, state = heapq.heappop(queue)
+            if prb.is_goal(state):
+                return Solution(state, prb, start_time)
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if c.__hash__() not in visited:
+                    visited.add(c.__hash__())
+                    heapq.heappush(queue, (c.g_n, c))
+        return None
+
