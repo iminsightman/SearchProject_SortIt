5a960ab52729228e2533fdc5e07d32d3ae7b60b1
elhamarmin <elhmarmin@gmail.com>
2023-03-29 15:59:29 +0430

    add rbfs

--- Search.py
+++ Search.py
@@ -100,7 +100,7 @@ class Search:
         return None
 
 
-  def dfs_limited(prb: Problem, depth: int) -> State:
+    def dfs_limited(prb: Problem, depth: int , start_time) -> Solution:
         stack = []
         state = prb.initState
         stack.append(state)
@@ -108,21 +108,59 @@ class Search:
         visited.add(state.__hash__())
         while len(stack) > 0:
             state = stack.pop()
-            if prb.is_goal(state):
-                return state
             if state.g_n < depth:
                 neighbors = prb.successor(state)
                 for c in neighbors:
                     if c.__hash__() not in visited:
                         visited.add(c.__hash__())
+                        if prb.is_goal(c):
+                            return Solution(c, prb, start_time)
                         stack.append(c)
         return None
 
-     def ids(prb: Problem) -> Solution:
+
+    def ids(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        depth = 0
+        while True:
+            result = dfs_limited(prb, depth , start_time )
+            if result is not None:
+                return result
+            depth += 1
+
+     def rbfs(prb: Problem) -> Solution:
             start_time = datetime.now()
-            depth = 0
-            while True:
-                result = dfs_limited(prb, depth)
-                if result is not None:
-                    return Solution(result, prb, start_time)
-                depth += 1
+            state = prb.initState
+            visited = set()
+            visited.add(state.__hash__())
+            solution, _ = rbfs1(prb ,state, float('inf'), visited,  start_time)
+            return solution
+
+
+    def rbfs1(state, f_limit, visited, prb, start_time):
+
+        if prb.is_goal(state):
+            return Solution(state, prb, start_time), 0
+        successors = prb.successor(state)
+        if not successors:
+            return None, float('inf')
+        for c in successors:
+            if c.__hash__() in visited:
+                continue
+            visited.add(c.__hash__())
+            c.f_n = prb.heuristic(c)
+        heap = [(c.f_n, c) for c in successors]
+        heapq.heapify(heap)
+        while True:
+            best_f_n, best = heapq.heappop(heap)
+            if best_f_n > f_limit:
+                return None, best_f_n
+            if len(heap) > 0:
+                alternative_f_n, alternative = heapq.heappop(heap)
+                heapq.heappush(heap, (alternative_f_n, alternative))
+                result, best.f_n = rbfs1(best, min(f_limit, alternative_f_n), visited, prb, start_time)
+            else:
+                result, best.f_n = rbfs1(best, f_limit, visited, prb, start_time)
+            if result:
+                return result, 0
+            heapq.heappush(heap, (best.f_n, best))
